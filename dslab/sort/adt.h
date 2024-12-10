36a1e830f2dd31822b9196e1da527d0f105f4a3b
Geethaa19 <100697393+Geethaa19@users.noreply.github.com>
2024-12-10 12:32:42 +0530

    Delete dslab directory

--- dslab/sort/adt.h
+++
@@ -1,9 +0,0 @@
-struct numberADT
-{
-    int arr[100],n;
-};
-void init(struct numberADT *N,int n);
-void insertElements (struct numberADT *N, int x[10]);
-void selSort(struct numberADT *N);
-void shellSort(struct numberADT *N);
-void display(struct numberADT *N);
